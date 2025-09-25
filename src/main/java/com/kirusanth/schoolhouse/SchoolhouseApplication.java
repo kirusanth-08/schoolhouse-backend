a76d5f4c76620a78a9e7a568ada35604093018a9
Kirusanth <kirusanth200110@gmail.com>
2025-09-25 18:30:01 +0530

    initializing the application

---
+++ src/main/java/com/kirusanth/schoolhouse/SchoolhouseApplication.java
@@ -0,0 +1,13 @@
+package com.kirusanth.schoolhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolhouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirusanth/schoolhouse/SchoolhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirusanth.schoolhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
